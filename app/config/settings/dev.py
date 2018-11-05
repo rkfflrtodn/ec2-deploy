47f51f713dbdd8ca2bd1593445e91b069c22558b
Son Dong Hwan <zxcehdghks@gmail.com>
2018-11-05 16:53:41 +0900

    User-upload file들을 저장할때 MediaStorage를 사용, S3 버킷에서 media/로 시작하는 경로에 저장

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -13,6 +13,9 @@ DATABASES = secrets['DATABASES']
 # django-storages
 # ~/.aws/credentials
 DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
+# collectstatic을 실행했을 때,
+# 버킷의 'static'폴더 아래에 정적파일들이 저장되도록 설정해보기
+#  config.storages.StaticStorage클래스를 만들어서 적용
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
