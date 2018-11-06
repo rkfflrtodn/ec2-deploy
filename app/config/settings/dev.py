c0fac231935020e4bfb14dc547958aafe0411598
Son Dong Hwan <zxcehdghks@gmail.com>
2018-11-06 11:12:41 +0900

    StaticStorage구현 및 default_acl설정

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -16,6 +16,11 @@ DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
 # collectstatic을 실행했을 때,
 # 버킷의 'static'폴더 아래에 정적파일들이 저장되도록 설정해보기
 #  config.storages.StaticStorage클래스를 만들어서 적용
+# STATICFILES_STORAGE = 'config.storages.StaticStorage'
+# 위 설정시 S3 프리티어의 기본 PUT 한계를 금방 초과하여 과금하게 되므로
+# STATIC_ROOT에 collectstatic후 Nginx에서 제공하는 형태로 사용
+
+
 AWS_ACCESS_KEY_ID = secrets['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = secrets['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = secrets['AWS_STORAGE_BUCKET_NAME']
